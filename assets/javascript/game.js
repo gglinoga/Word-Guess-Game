62d020cf398063fa2ddb913828c61d941fb46d74
Greg Glinoga <gglinoga@gmail.com>
2019-03-14 23:43:53 -0700

    almost done!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,89 @@
 var got = {
-    words: ["ned stark", "daenarys targaryen", "jamie lannister", "the hound", "the mountain", "the queen of thorns", "the wall", "white walkers", "ramsay bolton", "bobby b", "joffrey baratheon"],
+    //Variables -------------------------
+    words: ["stark", "baratheon", "daenarys", "lannister", "tyrell", "joffrey"],
+    chosenWord: "",
+    underline: [],
+    wordletter: [],
+    wrongletter: [],
+    blanks: 0,
+    wins: 0,
+    losses: 0,
+    wrongAnswers: 0,
+    rightAnswers: 0,
+    guessesleft: 7,
+    letter: "",
 
-    rng: function () {
-        rngvalue = (Math.floor(Math.random() * this.words.length));
-        chosenWord = this.words[rngvalue];
+    //Startup conditions -----------------------
+    startup: function () {
+        this.chosenWord = this.words[Math.floor(Math.random() * this.words.length)];
+        this.wordletter = this.chosenWord.split("");
+        this.blanks = this.wordletter.length;
+        this.wrongAnswers = 0;
+        this.rightAnswers = 0;
+        this.guessesleft = 7;
+        this.wrongletter = [];
+        this.underline = [];
+
+        for (i = 0; i < this.blanks; i++) {
+            this.underline.push("_");
+        }
+
+        document.getElementById("guessWord").innerHTML = this.underline.join(" ");
+        document.getElementById("guessesLeft").innerHTML = this.guessesleft;
+        document.getElementById("wins").innerHTML = this.wins;
+        document.getElementById("losses").innerHTML = this.losses;
+        document.getElementById("lettersGuessed").innerHTML = this.wrongletter;
+
+        console.log(this.chosenWord)
     },
-    wordlength: function () {
-        wordlengthV = this.words[rngvalue].length;
+
+    //Functions ---------------------------------
+    verify: function (letter) {
+        var LetterCheck = false
+        var letters = /[a-z]/;
+        for (i = 0; i < this.blanks; i++) {
+            if (this.chosenWord[i] == letter) {
+                LetterCheck = true;
+            }
+        }
+
+        if (LetterCheck) {
+            for (i = 0; i < this.blanks; i++) {
+                if (this.chosenWord[i] == letter) {
+                    this.underline[i] = letter;
+                    document.getElementById("guessWord").innerHTML = this.underline.join(" ");
+                }
+            }
+        }
+        else {
+            this.wrongletter.push(letter);
+            this.guessesleft--;
+            document.getElementById("lettersGuessed").innerHTML = this.wrongletter.join(" ");
+            document.getElementById("guessesLeft").innerHTML = this.guessesleft;
+        }
     },
-    underscore: function () {
-        var underscore = {};
-        for (i = 0; i < wordlengthV; i++) {
-            underscore.push("_");
-               return underscore;
+
+    roundover: function () {
+
+        if (this.wordletter.toString() === this.underline.toString()) {
+            this.wins++;
+            alert("You win!")
+            document.getElementById("wins").innerHTML = this.wins;
+            got.startup();
+        }
+        else if (this.guessesleft === 0) {
+            this.losses++;
+            alert("You lose!")
+            document.getElementById("losses").innerHTML = this.losses;
+            got.startup();
         }
     }
 }
-// testprint: function() {
-//     console.log("testprint" + this.words[this.rng()]);
-// }
+//-----------------start--------------
+got.startup()
 
-
-
-got.rng();
-got.wordlength();
-got.underscore();
-console.log(got.underscore())
-// console.log(chosenWord, wordlengthV);
-
-// document.onkeyup = function (e) {
-//     key = e.key
-//     for (var i = 0; i < got.rng(); i++) {
-//         var letter = (got.words[got.rng].charAt(i));
-//         if (key === letter) {
-//             console.log("Correct");
-//         }
-//         else {
-//             console.log("Incorrect");
-//         }
-//     }
-
-// }
+document.onkeyup = function (e) {
+    var letter = e.key;
+    got.verify(letter);
+    got.roundover();
+}
